26763a3fe2cee007454bb8032adeae18af581a4f
Umesh <umesh@gurzu.com>
2020-02-29 00:15:35 +0545

    Developement: Added test spec for the movie model and passed the test by validating movies attributes.

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -11,5 +11,10 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+	it { should validate_presence_of(:title) }
+	it { should validate_presence_of(:plot) }
+	it "is not valid without title" do 
+		movie = Movie.new(plot: nil)
+		expect(movie).to_not be_valid
+	end
 end
